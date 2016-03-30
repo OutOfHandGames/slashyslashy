fa303e42b4fa2a6636c028790c3719634514d5f8
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-03-30 00:13:07 -0400

    Scene created. Not fuctioning

---
+++ Assets/Scripts/WalkMechanics.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkMechanics : MonoBehaviour {
+    public float speed = 8;
+    public float acceleration = 10;
+
+    float hInput;
+    float vInput;
+    Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void horizontalInput(float hInput)
+    {
+        this.hInput = hInput;
+    }
+
+    public void verticalInput(float vInput)
+    {
+        this.vInput = vInput;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void updateMovement()
+    {
+        float goalSpeed = speed * hInput;
+        Vector2 goalVel = Vector2.right * goalSpeed + Vector2.up * rigid.velocity.y;
+        rigid.velocity = Vector2.MoveTowards(rigid.velocity, goalVel, Time.deltaTime * acceleration);
+
+    }
+}
