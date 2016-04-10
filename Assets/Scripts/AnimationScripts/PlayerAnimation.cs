0c40761c1b25d576ab7c7d096e438979cdc75e74
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-04-10 02:40:08 -0400

    Run and Idle animations added

---
+++ Assets/Scripts/AnimationScripts/PlayerAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+    Animator anim;
+    WalkMechanics walkMechanics;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        walkMechanics = transform.parent.GetComponent<WalkMechanics>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("Speed", Mathf.Abs(walkMechanics.getHInput()));
+	}
+}
