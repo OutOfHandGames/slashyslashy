4b16a04b04e9b01007864196cfb1cb5a3196c9ad
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-05-04 21:40:31 -0400

    Camera stable y Position

--- Assets/Scripts/CameraScripts/CameraFollow.cs
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -33,8 +33,31 @@ public class CameraFollow : MonoBehaviour {
         Vector3 goalPosition = Vector3.forward * zPosition;
         goalPosition += Vector3.up * (target.position.y + yOffset);
         goalPosition += Vector3.right * (target.position.x + xOffset);
-
+        goalPosition = adjustGoalPosition(goalPosition);
         transform.position = Vector3.SmoothDamp(transform.position, goalPosition, ref currentVel, cameraAcceleration);
+    }
+
+    Vector3 adjustGoalPosition(Vector3 oldPosition)
+    {
+        Vector3 newPosition = oldPosition;
+        if (newPosition.x > maxX)
+        {
+            newPosition = new Vector3(maxX, newPosition.y, newPosition.z);
+        }
+        else if(newPosition.x < minX)
+        {
+            newPosition = new Vector3(minX, newPosition.y, newPosition.z);
+        }
+
+        if (newPosition.y > maxY)
+        {
+            newPosition = new Vector3(newPosition.x, maxY, newPosition.z);
+        }
+        else if(newPosition.y < minY)
+        {
+            newPosition = new Vector3(newPosition.x, minY, newPosition.z);
+        }
+        return newPosition;
     }
 
     public void setTarget(Transform target)
